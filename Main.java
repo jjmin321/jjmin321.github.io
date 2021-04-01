684950972428396c362cbe9bc8e74ac059016fb5
jjmin321 <jjmin321@naver.com>
2021-04-01 13:41:08 +0900

    [UPDATE] POST

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Account account = new Account();
-        account.deposit(2000);
-        account.withdraw(1000);
-        account.deposit(3000);
-        account.withdraw(5000);
-    }
-}
